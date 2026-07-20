1ce7f8dd5a6697f2c633573d4b859cabaacc1d39
Kien Nguyen <besitin12@gmail.com>
2026-07-20 18:25:36 +0700

    config cors to add origins

--- Student Management System/Program.cs
+++ Student Management System/Program.cs
@@ -130,7 +130,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+            .WithOrigins("http://localhost:3000", "http://127.0.0.1:3000", "https://sms-fe-liart.vercel.app/")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
